9a71523f8e1d650fb2ebbb3f758efa2b75b54732
maria-sancho <alusan7934@ieselcaminas.org>
2023-02-02 14:02:38 +0100

    Invoice

--- src/Invoice.java
+++ src/Invoice.java
@@ -1,2 +1,73 @@
-package PACKAGE_NAME;public class Invoice {
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Invoice {
+    private int invoiceNumber;
+    private Date invoiceDate;
+    private String billingAddress;
+    private String shippingAddress;
+    private String salesPerson;
+    private String shippingMethod;
+    private String shippingTerms;
+    private Date deliveryDate;
+    private String paymentTerms;
+    private Date dueDate;
+    private float totalDiscount;
+    private List<InvoiceLine> lines;
+
+    public Invoice(int invoiceNumber, Date invoiceDate, String billingAddress, String shippingAddress,
+                   String salesPerson, String shippingMethod, String shippingTerms, Date deliveryDate,
+                   String paymentTerms, Date dueDate, float totalDiscount) {
+        this.invoiceNumber = invoiceNumber;
+        this.invoiceDate = invoiceDate;
+        this.billingAddress = billingAddress;
+        this.shippingAddress = shippingAddress;
+        this.salesPerson = salesPerson;
+        this.shippingMethod = shippingMethod;
+        this.shippingTerms = shippingTerms;
+        this.deliveryDate = deliveryDate;
+        this.paymentTerms = paymentTerms;
+        this.dueDate = dueDate;
+        this.totalDiscount = totalDiscount;
+        lines = new ArrayList<>();
+    }
+
+    public void addInvoiceLine(InvoiceLine line) {
+        lines.add(line);
+    }
+
+    public void print() {
+        System.out.println("Invoice number: " + invoiceNumber);
+        System.out.println("DATE: " + invoiceDate);
+        System.out.println("TO: " + billingAddress);
+        System.out.println("SHIP TO: " + shippingAddress);
+        System.out.println("...........................................................................................................................");
+        System.out.println("| SALES PERSON | SHIPPING METHOD | SHIPPING TERMS | DELIVERY DATE | PAYMENT TERMS | DUE DATE |");
+        System.out.println("...........................................................................................................................");
+        System.out.println(String.format("| %s | %s | %s | %s | %s | %s |", salesPerson, shippingMethod, shippingTerms, deliveryDate, paymentTerms, dueDate));
+        System.out.println("...........................................................................................................................");
+        System.out.println("");
+        System.out.println("...........................................................................................................................");
+        System.out.println("| QTY | Item # | DESCRIPTION | UNIT PRICE | DISCOUNT | LINE TOTAL |");
+        System.out.println("...........................................................................................................................");
+        for (InvoiceLine line : lines) {
+            System.out.println(line);
+        }
+        System.out.println("...........................................................................................................................");
+        float subtotal = 0;
+        for (InvoiceLine line : lines) {
+            subtotal += line.getTotal();
+        }
+        float discountAmount = subtotal * totalDiscount / 100;
+        float total = subtotal - discountAmount;
+        System.out.println("| TOTAL DISCOUNT | " + InvoiceLine.customFormat("###,###.00", totalDiscount) + "% | $" + InvoiceLine.customFormat("###,###.00", discountAmount) + " |");
+        System.out.println(".................");
+        System.out.println("| SUBTOTAL | $" + InvoiceLine.customFormat("###,###.00", subtotal) + " |");
+        System.out.println(".................");
+        System.out.println("| GST | $" + InvoiceLine.customFormat("###,###.00", total * 0.21) + " |");
+        System.out.println("-.................");
+        System.out.println("| TOTAL | $" + InvoiceLine.customFormat("###,###.00", total) + " |");
+        System.out.println(".................");
+    }
 }
--- src/InvoiceLine.java
+++ src/InvoiceLine.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class InvoiceLine {
+import java.text.DecimalFormat;
+
+public class InvoiceLine {
+    private int quantity;
+    private String itemCode;
+    private String itemDescription;
+    private float unitPrice;
+    private float discountRate;
+
+    public InvoiceLine(int quantity, String itemCode, String itemDescription, float unitPrice, float discountRate) {
+        this.quantity = quantity;
+        this.itemCode = itemCode;
+        this.itemDescription = itemDescription;
+        this.unitPrice = unitPrice;
+        this.discountRate = discountRate;
+    }
+
+    public float getTotal() {
+        return quantity * unitPrice * (1 - discountRate / 100);
+    }
+
+    public String toString() {
+        return String.format("          | %d | %s | %s | $%s | %.2f%% | $%s |",
+                quantity, itemCode, itemDescription, customFormat("###,###.00", unitPrice), discountRate, customFormat("###,###.00", getTotal()));
+    }
+
+    public static String customFormat(String pattern, double value ){
+        DecimalFormat myFormatter = new DecimalFormat(pattern);
+        String output = myFormatter.format(value);
+        return output;
+    }
 }
